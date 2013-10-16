a06ab812c22f367f2cf9538a4450325a930ee18f
Taylor Brushwyler <trb1992@gmail.com>
2013-10-16 16:16:27 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qooqbooq::Application.config.secret_key_base = '75caeed16785bdd3a62b964a0938ebef7b9f3d891ba15b7ca1c92a0d2df703092ec1cd9e152df87a0067a0f53fc2c4ec7b6656625f2a93aacdf94db94749f297'
